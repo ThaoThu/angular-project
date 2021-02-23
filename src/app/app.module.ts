aeb0fb7f5e84b2371ebc54d1e34a993bf4e23c08
Rosa <thaontt3@fintech.smartosc.com>
2021-02-23 10:01:49 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DirectivesComponent } from './directives/directives.component';
+import { Day1Component } from './day1/day1.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DirectivesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DirectivesComponent, Day1Component],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivesComponent implements OnInit {
   isRedColor = true;
-  userClass = { fontBold: 'txt--bold' };
+  userClass = { fontBold: true };
 
   constructor() {}
 
