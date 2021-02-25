df680319b7c76d37d3d8789fbaa31d7321c3e9d2
Rosa <thaontt3@fintech.smartosc.com>
2021-02-25 09:21:16 +0700

    adding router module

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -5,8 +5,9 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductRoutingModule } from './product-routing.module';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [ProductDetailComponent, ProductListComponent],
-  imports: [CommonModule, ProductRoutingModule, MatCardModule, MatGridListModule]
+  imports: [CommonModule, ProductRoutingModule, MatCardModule, MatGridListModule, RouterModule]
 })
 export class ProductModule { }
